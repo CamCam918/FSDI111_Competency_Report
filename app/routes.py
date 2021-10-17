8b1e3589d85b04bc87ef7a16f2320e97387695df
Cameron Campbell <cameron.campbell918@yahoo.com>
2021-10-17 12:09:41 -0700

    Competency Report before Details display

--- app/routes.py
+++ app/routes.py
@@ -3,14 +3,13 @@ from app import app, db
 from app.database import Message
 
 
-
-@app.route("/messages", method=["POST"])
+@app.post("/messages")
 def create_message():
     msg_data = request.json
     title= msg_data.get("title")
     body= msg_data.get("body")
     if not title or not body:
-        return "<h1>Invalisd syntax<?h1>", 400
+        return "<h1>Invalisd syntax</h1>", 400
     message = Message(title=title, body = body)
     db.session.add(message)
     db.session.commit()
@@ -18,16 +17,18 @@ def create_message():
 
 @app.get("/messages")
 def read_all_messages():
-    return Message.query.all()
+    messages = Message.query.all()
+    return render_template("message_list.html", message_list=messages )
 
 @app.get("/messages/<int:pk>")
 def read_message_by_id(pk):
-    return Message.query.filter_by(id=pk).first()
+    message = Message.query.filter_by(id=pk).first()
+    return render_template("message_detail.html", message=message)
 
 
-@app.get("greeting/<name>")
+@app.get("/greeting/<name>")
 def greet(name):
-    return render_template("home.html", name=name)
+    return render_template("/home.html", name=name)
 #implement the following operations:
 #1. create message
 #2. Scan (display all mesages)
